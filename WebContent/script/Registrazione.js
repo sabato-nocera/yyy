378c69ff191e477d696bab003ba47c2d98cc8d1d
Sabato Nocera <52103278+sabato-nocera@users.noreply.github.com>
2024-07-02 17:27:32 +0200

    Update Registrazione.js

--- WebContent/script/Registrazione.js
+++ WebContent/script/Registrazione.js
@@ -11,7 +11,7 @@ function checkNomeCognome(inputtxt) {
 
 
 function checkEmail(inputtxt) {
-	var email = /^\S+@\S+\.\S+$/;
+	var email = /^[A-Za-z0-9+_.-]+@(.+)$/;
 	if(inputtxt.value.match(email)) 
 		return true;
 	
